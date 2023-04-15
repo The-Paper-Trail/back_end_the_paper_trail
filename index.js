89bbbca5369aa5695364f4e5603356911a2115cf
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-04-15 08:56:10 +0000

    done

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
-const {Client} = require('pg');
+const { Client } = require('pg');
 
 const client = new Client(process.env.DATABASE);
 
 const app = express();
-
 app.use(cors());
 const port = process.env.PORT;
+const APIKey = process.env.API_KEY
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-client.connect().then(()=>{
+client.connect().then(() => {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   })
